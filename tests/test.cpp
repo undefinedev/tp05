b29e0d001ea4269fd8a9df7bf5aeb0955d184d95
undefinedev <undefined_dev@inbox.ru>
2022-04-06 23:21:46 +0300

    waiting for miracle

---
+++ tests/test.cpp
@@ -0,0 +1,77 @@
+// Copyright 2022 undefinedev Timofeev Vladislav
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <Account.h>
+#include <Transaction.h>
+
+using ::testing::Return;
+
+class MockAccount : public Account {
+public:
+    MockAccount(int id, int balance) : Account(id, balance) {}
+
+    MOCK_METHOD(int, GetBalance, (), (const, override));
+    MOCK_METHOD(void, ChangeBalance, (int diff), (override));
+    MOCK_METHOD(void, Lock, (), (override));
+    MOCK_METHOD(void, Unlock, (), (override));
+};
+
+//class MockTransaction : public Transaction {
+//    MockTransaction() : Transaction() {};
+//
+//    MOCK_METHOD(bool, Make, (Account& from, Account& to, int sum));
+//};
+
+TEST(BankTest, Bank) {
+    MockAccount t1(1, 220);
+    MockAccount t2(2, 100);
+    Transaction test;
+    EXPECT_CALL(t1, GetBalance())
+            .WillOnce(testing::Return(220))
+            .WillOnce(testing::Return(69))
+            .WillOnce(testing::Return(69))
+            .WillOnce(testing::Return(69));
+    EXPECT_CALL(t2, GetBalance())
+            .WillOnce(testing::Return(250))
+            .WillOnce(testing::Return(250));
+    EXPECT_CALL(t1, ChangeBalance(-151))
+            .Times(1);
+    EXPECT_CALL(t2, ChangeBalance(testing::_))
+            .Times(3);
+    EXPECT_CALL(t1, Lock())
+            .Times(5)
+            .WillOnce(testing::Throw(std::runtime_error("at first lock the account")))
+            .WillOnce(nullptr)
+            .WillOnce(testing::Throw(std::runtime_error("already locked")))
+            .WillOnce(nullptr)
+            .WillOnce(nullptr);
+    EXPECT_CALL(t2, Lock())
+            .Times(2);
+    EXPECT_CALL(t1, Unlock())
+            .Times(3);
+    EXPECT_CALL(t2, Unlock())
+            .Times(2);
+
+    EXPECT_THROW(test.Make(t1, t1, 50), std::logic_error);
+    EXPECT_THROW(test.Make(t1, t2, 50), std::logic_error);
+    EXPECT_THROW(test.Make(t1, t2, -50), std::invalid_argument);
+
+    test.set_fee(200);
+    EXPECT_EQ(test.Make(t1, t2, 150), 0);
+    EXPECT_EQ(test.fee(), 200);
+    test.set_fee(1);
+
+    EXPECT_THROW(test.Make(t1, t2, 150), std::runtime_error);
+    t1.Lock();
+    EXPECT_THROW(test.Make(t1, t2, 150), std::runtime_error);
+    t1.Unlock();
+
+    bool res = test.Make(t1, t2, 150);
+    EXPECT_EQ(res, 1);
+
+    res = test.Make(t1, t2, 150);
+    EXPECT_EQ(res, 0);
+
+}
